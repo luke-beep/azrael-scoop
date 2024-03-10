647e60ea87feb8e683fd70a24437ab8ba14646f5
luke-beep <lukehjo@gmail.com>
2024-03-10 23:02:05 +0100

    main -> master

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,5 @@
 param(
-    # overwrite upstream param
-    [String]$upstream = "luke-beep/azrael-scoop:main"
+    [String]$upstream = "luke-beep/azrael-scoop:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
